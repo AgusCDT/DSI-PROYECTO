145f80b97a45f21bf9426eff076bc49ae31cb1cd
Cao33 <pablocao@ucm.es>
2023-04-24 00:36:02 +0200

    Bases para el terreno de juego

--- Meditics/Meditics/Match.xaml.cs
+++ Meditics/Meditics/Match.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -47,6 +48,26 @@ namespace Meditics
         private void Defeat_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(EndGame), false);
+        }
+
+        private void Image_DragStarting(UIElement sender, DragStartingEventArgs args)
+        {
+            int numchilds = Terreno.Children.Count();
+            for (int i = 0; i < numchilds; i++)
+            {
+                Grid child = Terreno.Children[i] as Grid;
+                child.Background = new SolidColorBrush(Windows.UI.Colors.Blue);
+            }
+        }
+
+        private void Image_DropCompleted(UIElement sender, DropCompletedEventArgs args)
+        {
+            int numchilds = Terreno.Children.Count();
+            for (int i = 0;i < numchilds; i++) 
+            {
+                Grid child= Terreno.Children[i] as Grid;
+                child.Background= new SolidColorBrush(Windows.UI.Colors.Transparent);
+            }
         }
     }
 }
